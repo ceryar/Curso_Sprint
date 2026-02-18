e5283d881e6771248418ee39cc60a71a3bfe8354
Cesar Castro <ceryar@gmail.com>
2026-02-18 11:33:33 -0500

    Aplicando template en la paguina principal

--- src/main/java/com/pruebaTiket/tiket/controller/WelcomeController.java
+++ src/main/java/com/pruebaTiket/tiket/controller/WelcomeController.java
@@ -12,15 +12,15 @@ import com.pruebaTiket.tiket.model.User;
 public class WelcomeController {
 
 
-    @GetMapping
+    @GetMapping("/")
     public String wolcome( Model model){
 
         model.addAttribute("name","Walcome to Prueba1");//pasando parametros a la vista.
         User user1 = new User("Cesar castro", "cesar@gmail.com");
         User user2 = new User("Antonio Hurtado", "antonio@gmail.com");
         User user3 = new User("Felipe Lopez", "felipe@gmail.com");
-        //List<User>  users = List.of(user1, user2, user3); // se crea una nueva lista de usuarios 
-        List<User>  users = List.of(); //lista sin usuarios
+        List<User>  users = List.of(user1, user2, user3); // se crea una nueva lista de usuarios 
+        //List<User>  users = List.of(); //lista sin usuarios
         model.addAttribute("users", users);// se pasa lista de usuarios con la variable "users"
 
         return "welcome";
