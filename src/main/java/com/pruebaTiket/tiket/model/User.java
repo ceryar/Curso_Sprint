157ab05514bbb9e33cbe00ca9bb3f2e77bfdef18
Cesar Castro <ceryar@gmail.com>
2026-02-18 17:36:28 -0500

    manpeando model User para crear tabla en la base de datos

--- src/main/java/com/pruebaTiket/tiket/model/User.java
+++ src/main/java/com/pruebaTiket/tiket/model/User.java
@@ -1,18 +1,21 @@
 package com.pruebaTiket.tiket.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-
+/*Con lombok solo se define los atributos de las clases
+ */
 @Getter // para que la clase Lombook cree kis get
 @Setter // para que la clase Lombook cree kis set
 @NoArgsConstructor // crea construtor sin parametros
 @AllArgsConstructor // constructor con parametros
 @Builder
-
-/*Con lombok solo se define los atributos de las clases
- */
-
+@Entity//crear tabla en la base de datos
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
